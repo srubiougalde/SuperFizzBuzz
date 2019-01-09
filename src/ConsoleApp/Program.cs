31ac8da338877c63f3890670f515c00bda573fd1
Sergio Rubio <Sergio.Rubio@wolterskluwer.com>
2019-01-09 17:14:19 -0600

    Update console app to call both classic and super fizzbuzz

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Headspring.FizzBuzz;
 
 namespace ConsoleApp
@@ -7,10 +8,30 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            ClassicFizzBuzz();
+            SuperFizzBuzz();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+
+        static void ClassicFizzBuzz()
+        {
             var fizzbuzz = new ClassicFizzBuzz();
-            var result = fizzbuzz.Evaluate(2, 35); 
-            // var result = fizzbuzz.Evaluate(-35, -2); 
-            // var result = fizzbuzz.Evaluate(1, 1000000000);
+            var result = fizzbuzz.Evaluate(1, 100); 
+
+            Console.WriteLine("{0}", result);
+        }
+
+        static void SuperFizzBuzz()
+        {
+            var tokens = new List<KeyValuePair<int, string>>();
+            tokens.Add(new KeyValuePair<int, string>(3, "Fizz"));
+            tokens.Add(new KeyValuePair<int, string>(7, "Buzz"));
+            tokens.Add(new KeyValuePair<int, string>(38, "Bazz"));
+
+            var fizzbuzz = new SuperFizzBuzz(tokens);
+            var result = fizzbuzz.Evaluate(-12, 145); 
 
             Console.WriteLine("{0}", result);
         }
